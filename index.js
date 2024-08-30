02b10c70266210d432b30cd35554f5b3c4316021
Lucas Gomes <lucasagostini400@gmail.com>
2024-08-29 22:51:24 -0300

    Aprendendo sobre if, else if e else no Javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+let nomeFilme = 'Capitão America'
+
+if(nomeFilme === 'Vingadores'){
+    console.log(('É o filme dos Vingadores'))
+}else if(nomeFilme === 'Batman vs Superman'){
+    console.log('É o filme do Batman vs Superman')
+}else{
+    console.log('É outro filme')
+}
+
+// condicao ? console.log('True') : console.log('False')
+
+false ? console.log('Condição verdadeira') : console.log('Condição falsa')
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos Vingadores') : console.log('É outro filme')
